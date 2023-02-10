5ba0f996d6b633aff1ed9d586c760111b75dd697
zaiky09 <zamilmozamil@outlook.com>
2023-02-10 08:43:50 +0300

    Tests passing

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,27 @@ require 'pry'
 class String
 
   def sentence?
+    self.end_with?(".")
     
   end
 
   def question?
+    self.end_with?("?")
 
   end
 
   def exclamation?
+    self.end_with?("!")
 
   end
 
   def count_sentences
+    split = self.split(/[.!?]+/)
+    split.reject! { |word| word.strip.empty? }
+    split.count
+
+    #binding.pry
+
 
   end
 end
